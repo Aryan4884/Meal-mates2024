bbdec4d29abd4e9e8ca7be59c5283e81aae14e0a
Aryan Raj <116114086+Aryan4884@users.noreply.github.com>
2024-07-10 20:37:45 +0530

    Update index.js

--- Meal-backend/index.js
+++ Meal-backend/index.js
@@ -2,16 +2,20 @@ import dotenv from "dotenv";
 import { app } from "./app.js";
 import connectDB from "./db/index.js";
 
+// Load environment variables from .env file
 dotenv.config({ path: "./.env" });
 
+// Connect to MongoDB
 connectDB()
   .then(() => {
     const port = process.env.PORT || 8000;
 
+    // Start server
     app.listen(port, () => {
       console.log(`Server is running on PORT ${port}`);
     });
   })
   .catch((error) => {
     console.error("Connection Error:", error);
+    process.exit(1); // Exit process with failure
   });
